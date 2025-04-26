2bf6917b210e7ad45d0572b677ae3776b24fe618
mehedipro3 <mehediratul37@gmail.com>
2025-04-26 10:24:10 +0600

    Category Added

---
+++ src/Pages/Category.jsx
@@ -0,0 +1,52 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+import slide1 from '../assets/home/slide1.jpg';
+import slide2 from '../assets/home/slide2.jpg';
+import slide3 from '../assets/home/slide3.jpg';
+import slide4 from '../assets/home/slide4.jpg';
+import slide5 from '../assets/home/slide5.jpg';
+
+const Category = () => {
+  return (
+    <>
+      <Swiper
+        slidesPerView={4}
+        centeredSlides={true}
+        spaceBetween={30}
+        grabCursor={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper mb-12"
+      >
+        <SwiperSlide>
+          <img src={slide1}></img>
+          <h2 className='text-2xl uppercase text-center font-bold text-white -mt-16'>Salads</h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2}></img>
+          <h2 className='text-2xl uppercase text-center font-bold text-white -mt-16'>pizza</h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3}></img>
+          <h2 className='text-2xl uppercase text-center font-bold text-white -mt-16'>soup</h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4}></img>
+          <h2 className='text-2xl uppercase text-center font-bold text-white -mt-16'>cake</h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5}></img>
+          <h2 className='text-2xl uppercase text-center font-bold text-white -mt-16'> Salad </h2>
+        </SwiperSlide>
+        
+      </Swiper>
+    </>
+  );
+};
+
+export default Category;
--- src/Pages/Homes.jsx
+++ src/Pages/Homes.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import Category from './Category';
 
 const Homes = () => {
   return (
     <div>
       <Banner></Banner>
+      <Category></Category>
     </div>
   );
 };
