1c062b408dceb2942791ad84611fed0860652a32
mehedipro3 <mehediratul37@gmail.com>
2025-08-05 23:59:26 +0600

    user home added

--- src/Pages/Dashboard/UserHome/UserHome.jsx
+++ src/Pages/Dashboard/UserHome/UserHome.jsx
@@ -4,12 +4,7 @@ const UserHome = () => {
   const { user } = useAuth();
   return (
     <div>
-        <h2 className='text-3xl'>
-          <span>Hi! Welcome </span>
-          {
-            user?.displayName ? user.displayName : 'Back' 
-          }
-        </h2> 
+        
     </div>
   );
 };
