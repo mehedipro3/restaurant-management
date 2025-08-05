917aeb9fa2a16e2f8f08d5a4fededc34c1b08f42
mehedipro3 <mehediratul37@gmail.com>
2025-08-06 01:31:41 +0600

    upadte admin home

--- src/Pages/Dashboard/UserHome/UserHome.jsx
+++ src/Pages/Dashboard/UserHome/UserHome.jsx
@@ -4,7 +4,12 @@ const UserHome = () => {
   const { user } = useAuth();
   return (
     <div>
-        
+        <h2 className='text-3xl'>
+          <span>Hi! Welcome </span>
+          {
+            user?.displayName ? user.displayName : 'Back' 
+          }
+        </h2> 
     </div>
   );
 };
